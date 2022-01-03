4ef17f8f1a97384e6302c3e355df361a8467ecb6
ugattem <ugattem@iu.edu>
2022-01-03 05:14:49 -0500

    Updated Projects page

--- js/custom_scripts.js
+++ js/custom_scripts.js
@@ -12,3 +12,19 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+var col2 = document.getElementsByClassName("project_collapsible");
+var j;
+
+for (j = 0; j < col2.length; j++) {
+  col2[j].addEventListener("click", function() {
+    this.classList.toggle("project_collapse_active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
